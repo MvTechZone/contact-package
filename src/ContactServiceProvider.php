d2c981fbfb9eb7fdb2321ec7db229111ff59a89d
TechGuy <dev.techguy@gmail.com>
2019-05-29 16:24:07 +0300

    Updates...

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -10,10 +10,28 @@ class ContactServiceProvider extends ServiceProvider {
         $this->loadViewsFrom(__DIR__ . '/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->mergeConfigFrom(
-            __DIR__.'/config/contact.php', 'contact'
+            __DIR__ . '/config/contact.php', 'contact'
         );
+
+        //publishing configurations of the package
         $this->publishes([
-            __DIR__.'/config/contact.php' => config_path('contact.php')
+            __DIR__ . '/config/contact.php' => config_path('contact.php'),
+        ]);
+
+        //publishing migrations here
+        $this->publishes([
+            __DIR__ . '/database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        //publishing models
+        $this->publishes([
+            __DIR__ . '/Models' => public_path('app'),
+        ]);
+
+
+        //publishing views here
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/contact'),
         ]);
     }
 
